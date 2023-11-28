f83046d15a23f413ae2c72d875f0f5591ecb726b
if22b086@technikum-wien.at <lilligruber02@gmail.com>
2023-11-28 10:09:24 +0100

    Sample level and menu

---
+++ Tilt Five Racing Game/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RestartGame()
+    {
+        string currentSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentSceneName);
+    }
+}
